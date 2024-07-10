fac1257ed172d91b1141131f8269c542f09861de
mattraydev <cedaring.xr@gmail.com>
2024-07-10 07:26:06 -0600

    updated blog url

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -124,7 +124,7 @@ export default function ProjectsPage() {
 							{ name: 'Tailwind', url: Tailwind },
 							{ name: 'Typescript', url: Typescript }
 						]}
-						link="https://next-blog-template-git-main-matthew-rays-projects.vercel.app/"
+						link="https://www.radiantpine-devblog.com/"
 					/>
 					<Project
 						title="Ittero"
